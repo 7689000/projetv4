1bb9d061747f0b82194f7763decddb7770a3958b
7689000 <60218169+7689000@users.noreply.github.com>
2021-11-30 22:10:06 +0100

    commit

---
+++ src/Pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Topbar from '../Components/Topbar';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <Topbar/>
+        </div>
+    );
+};
+
+export default Dashboard;
---
+++ src/components/Icon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "D:/OCRES/WebTech/projetv4/src/Style/Icon.css";
+
+const Icon = () => {
+    return (
+        <div className="icon">
+            <img src="./img/FleeebzIcon.svg" alt="Logo"></img>
+        </div>
+    );
+};
+
+export default Icon;
